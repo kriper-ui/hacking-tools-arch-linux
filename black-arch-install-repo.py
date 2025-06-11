5781eb6005e3b6c4d20df1511179fa7e7569aa2a
kriper-ui <whoareyou2000221@gmail.com>
2025-06-11 16:46:14 +0100

    Update black-arch-install-repo.py

--- black-arch-install-repo.py
+++ black-arch-install-repo.py
@@ -1,7 +1,12 @@
 import os
 import time
 
+perm = os.getuid()
 
+if perm != 0:
+    print("Script may be run how root.")
+else:
+    
 print("Hello user this is a script for arch-linux to hacking!")
 
 print("Ok type 1 if you want install hyperland config, 2 if you want to install tools to hacking and 3 to install yay")
@@ -9,15 +14,9 @@ print("Ok type 1 if you want install hyperland config, 2 if you want to install 
 option = int(input())
 
 if option == 1:
-
-    print("First let's install git to download IMPORTANTE THAT IT NEED SOMETIMES RUN WITH SUDO!")
+    print("Installing process start")
     print("Do you have yay installed? enter 1 if yes and 0 if no")
-    yay = int(input())
-    
-    if yay == 0:
-        os.system("sudo pacman -Syu")
-    else:
-        os.system("yay")
+    os.system("sudo pacman -Syu")
     os.system("sudo pacman -S hyperland")
     os.system("sudo pacman -S git")
     os.system("git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git ~/Arch-Hyprland")
@@ -25,8 +24,7 @@ if option == 1:
     os.system("chmod u+x install.sh && ./install")
 
 elif option == 2:
-
-    print("Ok let's do that IMPORTANTE THAT NEED THEN ENTER SUDO PASSWORD")
+    print("Process start")
     os.mkdir("file-repo")
     os.chdir("file-repo")
     os.system("wget https://blackarch.org/strap.sh")
@@ -34,16 +32,14 @@ elif option == 2:
     time.sleep(30)
     print("Starting installing and I will install virtualbox if you want to practice with vulnhub or install kali!")
     os.system("sudo pacman -S nmap dirb metasploit wpscan hydra john netcat nikto ghidra virtualbox")
-    print("I install basics tools to do CTF you must install more if you want!")
+    print("Install of basics tools to do CTF was installed if you want to install more you can do it with pacman.")
 
 elif option == 3:
-
-    print("Let's install yay IMPORTANTE THAT IT NEED SOMETIMES RUN WITH SUDO!")
+    print("Install process was started.")
     os.system("sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si")
     time.sleep(35)
     print("Yay was installed correct you can use it!!")
 
 else:
-
     print("wrong option")
     return
